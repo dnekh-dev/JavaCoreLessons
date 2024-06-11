e7ec5ee82ff601e1d2356813670c62fc1d78dbb8
d_nekh <dnekh.dev@gmail.com>
2024-06-11 14:25:18 +0900

    Added operetion selection and handling mechanism in Main class

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Calculator calculator = new Calculator();
-
-        System.out.println(calculator.sum(5, 10));
-        System.out.println(calculator.mult(5, 10));
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome! Choose an action:\n\t1 - for addition\n\t2 - for multiplication");
+        String input = scanner.nextLine();
+        System.out.println("Enter two integers:");
+        int firstNum = scanner.nextInt();
+        scanner.nextLine();
+        int secondNum = scanner.nextInt();
+        if (input.equals("1")) {
+            System.out.println("Result of addition is " + calculator.sum(firstNum, secondNum));
+        } else {
+            System.out.println("Result multiplication is " + calculator.mult(firstNum, secondNum));
+        }
     }
 }

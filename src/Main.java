6c34eaccf43f76a3add0ab3388010f1ed15ae8e3
d_nekh <dnekh.dev@gmail.com>
2024-06-11 14:08:59 +0900

    Creaeted an instance of Claculator in Main class and printed the results of sum and mult methods to the console

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+
+        System.out.println(calculator.sum(5, 10));
+        System.out.println(calculator.mult(5, 10));
+    }
+}

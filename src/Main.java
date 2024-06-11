2ecb2df3783b8db2ccabfe2be74205fd54f1b799
d_nekh <dnekh.dev@gmail.com>
2024-06-11 20:11:22 +0900

    Main class was refactored with switch-case construction.

--- src/Main.java
+++ src/Main.java
@@ -14,28 +14,35 @@ public class Main {
                     3 - for raising a number to a power
                     4 - for calculate factorial
                     5 - obtain absolute value of number""");
-        String input = scanner.nextLine();
-        if ("1".equals(input) || "2".equals(input) || "3".equals(input)) {
-            System.out.println("Enter two integers:");
-            int firstNum = scanner.nextInt();
-            int secondNum = scanner.nextInt();
 
-            if ("1".equals(input)) {
-                System.out.println("Result of addition is " + calculator.sum(firstNum, secondNum));
-            } else if ("2".equals(input)) {
-                System.out.println("Result multiplication is " + calculator.mult(firstNum, secondNum));
-            } else {
-                System.out.println("Result of raising first number to a power of second number is " + calculator.pow(firstNum, secondNum));
-            }
-        } else if ("4".equals(input) || "5".equals(input)) {
-            System.out.println("Enter a number:");
-            int number = scanner.nextInt();
+        String userChoice = scanner.nextLine();
 
-            if ("4".equals(input)) {
-                System.out.println(number + "! is " + calculator.factorial(number));
-            } else {
-                System.out.println("Absolute value of your number is " + calculator.abs(number));
-            }
+        switch(userChoice) {
+            case "1", "2", "3":
+                System.out.println("Enter two integers:");
+                int firstNum = scanner.nextInt();
+                int secondNum = scanner.nextInt();
+
+                if ("1".equals(userChoice)) {
+                    System.out.println("Result of addition is " + calculator.sum(firstNum, secondNum));
+                } else if ("2".equals(userChoice)) {
+                    System.out.println("Result multiplication is " + calculator.mult(firstNum, secondNum));
+                } else {
+                    System.out.println("Result of raising first number to a power of second number is " + calculator.pow(firstNum, secondNum));
+                }
+                break;
+            case "4", "5":
+                System.out.println("Enter a number:");
+                int number = scanner.nextInt();
+
+                if ("4".equals(userChoice)) {
+                    System.out.println(number + "! is " + calculator.factorial(number));
+                } else {
+                    System.out.println("Absolute value of your number is " + calculator.abs(number));
+                }
+                break;
+            default:
+                System.out.println("There is no such option!");
         }
     }
 }
